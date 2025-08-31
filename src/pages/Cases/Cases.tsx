c330aeafa2a221c94a3d025d96d6e9fc4fa0c972
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-08-30 22:30:06 -0300

    ajustes acessibilidade cases

--- src/pages/Cases/Cases.tsx
+++ src/pages/Cases/Cases.tsx
@@ -119,6 +119,7 @@ const Cases = () => {
             id="cases"
             className={styles.scrollWrapper}
             ref={wrapperRef}
+            aria-label="Seção de projetos e cases profissionais"
         >
             <div
                 className={styles.container}
