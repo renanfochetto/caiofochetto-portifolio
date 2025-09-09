f1831ffc841eb4b3cfbcdf035960af88b8bfada1
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-09-08 22:02:18 -0300

    build: add rollup-plugin-visualizer and vite-plugin-inspect to enhance build insights

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import Inspect from 'vite-plugin-inspect';
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [
+    react(),
+    Inspect(),
+    visualizer({
+      filename: 'stats.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+    {
+      name: 'async-css-loader',
+      transformIndexHtml(html) {
+        return html.replace(
+          /<link rel="stylesheet" crossorigin href="(.*?)">/,
+          `
+<link rel="preload" href="$1" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="$1"></noscript>
+          `.trim()
+        );
+      }
+    }
+  ],
 });
